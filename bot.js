6f576483132c60159917e37f24e29c2bc3088780
prajval09 <145696791+prajval09@users.noreply.github.com>
2024-08-10 12:43:59 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Replace with your actual token
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = '7341405305:AAHX02Pgqgg1tjK4mn4uKFL_SqPnbf_0srQ';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Welcome! Click the link to access the web application: https://shoptypeassignment-1.onrender.com/');
+  bot.sendMessage(chatId, 'Welcome! Click the link to access the web application by Prajwal: https://shoptypeassignment-1.onrender.com/');
 });
 
 console.log('Bot is running...');
